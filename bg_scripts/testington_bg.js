402b0000ec9922ebc7ee2c15a029eb9b843e36d3
Johannes Coetzee <johannes@jemstep.com>
2019-09-17 07:38:14 +0200

    Fixed a bug

--- bg_scripts/testington_bg.js
+++ bg_scripts/testington_bg.js
@@ -1,6 +1,8 @@
 const runningList = new Set();
 
 function receivedMessage(content, sender, respond) {
+  console.log("Received message");
+  console.log(content);
   if (content.message == "START_AUTORUN") {
     console.log("Starting");
     runningList.add(content.uniqueName);
@@ -10,12 +12,13 @@ function receivedMessage(content, sender, respond) {
     });
   } else if (content.message == "STOP_AUTORUN") {
     console.log("Stopping");
+    runningList.delete(content.uniqueName);
     respond({
       message: content.message,
       result: "OK"
     });
   } else if (content.message == "ALREADY_RUNNING") {
-    console.log('Already running');
+    console.log("Already running");
     respond({
       message: content.message,
       result: `${runningList.has(content.uniqueName)}`
